e6462159fdd8d01f6b105dc193c7d0dd9f2c962b
Ilian Nilsson <ilian.nilsson@live.se>
2026-08-27 09:17:22 +0200

    Name the crate after its repository; take what this module owns

---
+++ target/debug/deps/xmip_core_assign-964efffbfa511cf2.d
@@ -0,0 +1,7 @@
+D:\Repos\Xmip\modules\capabilities\assign\target\debug\deps\xmip_core_assign-964efffbfa511cf2.d: src\lib.rs
+
+D:\Repos\Xmip\modules\capabilities\assign\target\debug\deps\libxmip_core_assign-964efffbfa511cf2.rlib: src\lib.rs
+
+D:\Repos\Xmip\modules\capabilities\assign\target\debug\deps\libxmip_core_assign-964efffbfa511cf2.rmeta: src\lib.rs
+
+src\lib.rs:
